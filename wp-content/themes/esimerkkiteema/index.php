c148034afe57eddc5d77b74a5a85aea71b9c249f
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-09 10:09:01 +0300

    Update

--- wp-content/themes/esimerkkiteema/index.php
+++ wp-content/themes/esimerkkiteema/index.php
@@ -1,13 +1,45 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
+<?php
+get_header();
+?>
+    <section class="hero">
+        <div class="hero-text">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post();
+                    the_title('<h1>', '</h1>');
+                    the_content();
+                endwhile;
+            else :
+                _e('Sorry, no posts matched your criteria.', 'ilen-esimerkki');
+            endif;
+            ?>
+        </div>
+        <img src="map.svg" alt="Hero">
+    </section>
+    <main>
+        <section class="products">
+            <h2>Featured Products</h2>
+            <article class="product">
+                <img src="//placehold.it/200x200?text=Product" alt="Product">
+                <h3>Product 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.</p>
+                <a href="#">Read More</a>
+            </article>
+            <article class="product">
+                <img src="//placehold.it/200x200?text=Product" alt="Product">
+                <h3>Product 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.</p>
+                <a href="#">Read More</a>
+            </article>
+            <article class="product">
+                <img src="//placehold.it/200x200?text=Product" alt="Product">
+                <h3>Product 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quos.</p>
+                <a href="#">Read More</a>
+            </article>
+        </section>
+    </main>
+<?php
+get_sidebar();
+get_footer();
