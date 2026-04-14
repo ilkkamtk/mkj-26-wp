3269806d1906fca82ebc158a5479cc74e6c5110c
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-14 15:33:59 +0300

    Add category products template with header and subcategory display

---
+++ wp-content/themes/esimerkkiteema/category-products.php
@@ -0,0 +1,38 @@
+<?php
+global $wp_query;
+get_header();
+?>
+    <section class="hero">
+        <div class="hero-text">
+            <?php
+            echo "<h1>" . single_cat_title("", false) . "</h1>";
+            echo "<p>" . category_description() . "</p>";
+            ?>
+        </div>
+        <?php
+        $header_images = get_uploaded_header_images();
+        $image_url = array_pop($header_images)['url'];
+        ?>
+        <img src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>">
+    </section>
+    <main>
+        <section class="products">
+            <?php
+            $args = [
+              "child_of" => get_queried_object_id(),
+              "hide_empty" => true,
+            ];
+            $subcategories = get_categories($args);
+            print_r($subcategories);
+            ?>
+
+
+            <h2><?php single_cat_title(); ?></h2>
+            <?php
+            generate_articles($wp_query);
+            ?>
+        </section>
+    </main>
+<?php
+get_sidebar();
+get_footer();
