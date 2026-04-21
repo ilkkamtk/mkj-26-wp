0b47c4facf9e92620796d26f0b8d3ebd5fe42a02
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-21 14:54:31 +0300

    Update single post AJAX loading to display title and content in modal

--- wp-content/themes/esimerkkiteema/js/singlePost.js
+++ wp-content/themes/esimerkkiteema/js/singlePost.js
@@ -5,19 +5,22 @@ const openModalButtons = jQuery('.open-modal');
 
 const getSinglePost = (id) => {
   const url = singlePost.ajax_url;
-  const data = new URLSearchParams({
+  const data = {
     action: 'single_post',
     post_id: id,
-  });
+  };
 
   jQuery.post(url, data).done((response) => {
-    console.log(response);
+    modalContent.empty();
+    modalContent.html(`<h2>${response.post_title}</h2>`);
+    modalContent.append(response.post_content);
+    modal[0].showModal();
   });
 };
 
 openModalButtons.on('click', (evt) => {
   evt.preventDefault();
-  getSinglePost(evt.target.dataset.id)
+  getSinglePost(evt.target.dataset.id);
 });
 
 closeBtn.on('click', () => {
